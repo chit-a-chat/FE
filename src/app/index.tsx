054388594e78d9e2047fa67da1d9ef798f7709e1
hani2057 <hanikim2057@gmail.com>
2024-07-19 22:02:06 +0900

    [feat] globalStyle 설정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+import { Global } from "@emotion/react";
+
 import { Providers } from "./provider";
 import { AppRouter } from "./router";
+import { globalStyle } from "./ui";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Providers>
+            <Global styles={globalStyle} />
             <AppRouter />
         </Providers>
     </React.StrictMode>
