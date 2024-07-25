b92a528e9c1a132097c101ad1e00aa3c2d6254ad
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-07-25 23:37:57 +0900

    [feat] axios 기본 설정 추가
    - axios 기본 설정 추가

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+import { changeAxiosConfig } from "@shared/lib/axios/changeAxiosConfig";
+
 import { Global } from "@emotion/react";
 
 import { Providers } from "./provider";
 import { AppRouter } from "./router";
 import { globalStyle } from "./ui";
+
+/** axios 설정 변경 */
+changeAxiosConfig();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
