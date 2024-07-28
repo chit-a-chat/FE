87c78ab3615e1e285c779faae6846161dfe9d351
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-07-28 14:35:25 +0900

    [doc] mock폴더 build 제외 - mock 폴더 build 제외

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+import react from "@vitejs/plugin-react";
+
+const filesExclude = [/^mock\//];
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    build: {
+        rollupOptions: {
+            external: [...filesExclude],
+        },
+    },
 });
