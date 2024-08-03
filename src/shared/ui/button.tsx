02b15b07740c8223524de5799f0663be9007a26d
hani2057 <hanikim2057@gmail.com>
2024-08-03 16:36:51 +0900

    [feat] 테마에 spacing 추가 및 버튼에 적용

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -10,14 +10,23 @@ interface ButtonProps {
     iconLeft?: "check";
     iconRight?: "check";
     to?: string;
+    // TODO: 버튼 사이즈 'large', 'small'인 경우 추가
+    size?: "medium";
 }
 
-export function Button({ variant = "primary", label, iconLeft, iconRight, to }: ButtonProps) {
+export function Button({
+    variant = "primary",
+    label,
+    iconLeft,
+    iconRight,
+    to,
+    size = "medium",
+}: ButtonProps) {
     const btnStyle = (theme: Theme) =>
         css({
-            padding: "10px 20px",
-            borderRadius: "8px",
-            gap: "6px",
+            padding: theme.button.padding[size],
+            borderRadius: theme.button.radius[size],
+            gap: theme.button.gap[size],
             cursor: variant === "disable" ? "auto" : "pointer",
             backgroundColor: theme.button.bgColor.default?.[variant],
             // border를 안쪽으로 주기 위해 boxShadow 사용함
@@ -39,9 +48,7 @@ export function Button({ variant = "primary", label, iconLeft, iconRight, to }: 
                 top: variant === "primary" || variant === "error" ? "-3px" : "0px",
                 width: `calc(100% + ${variant === "primary" || variant === "error" ? 4 : 0}px)`,
                 height: `calc(100% + ${variant === "primary" || variant === "error" ? 6 : 0}px)`,
-                padding: "3px 2px",
-                borderRadius: "8px",
-                gap: "6px",
+                borderRadius: theme.button.radius[size],
             },
 
             "&:active": {
--- src/shared/ui/theme/theme.ts
+++ src/shared/ui/theme/theme.ts
@@ -1,5 +1,19 @@
 import { lightPalette } from "./palette";
 
+const spacing = {
+    xl: "32px",
+    lg: "20px",
+    "md-lg": "18px",
+    md: "16px",
+    "sm-md": "14px",
+    sm: "12px",
+    xs: "10px",
+    "2xs": "8px",
+    "3xs": "6px",
+    "4xs": "4px",
+    "5xs": "2px",
+};
+
 const light = {
     // app background color
     bgColor: "linear-gradient(180deg, rgba(231, 225, 255, 0.4) 0%, rgba(221, 230, 253, 0.4) 83%)",
@@ -39,7 +53,21 @@ const light = {
                 disable: lightPalette.grey[3],
             },
         },
+        // padding
+        padding: {
+            // TODO: 버튼 사이즈에 따라 다르게 지정 2024.08.03. 김하늬
+            medium: `${spacing.xs} ${spacing.lg}`,
+        },
+        // border-radius
+        radius: {
+            medium: spacing["2xs"],
+        },
+        // gap between button icon and label
+        gap: {
+            medium: spacing["3xs"],
+        },
     },
+    spacing,
 };
 
 export const theme = { light };
