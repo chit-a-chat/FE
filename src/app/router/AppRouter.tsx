aa7156edeb967c778ebb65ead4238ce92423620e
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-08-04 19:01:26 +0900

    [feat] Example 라우팅 추가 - Example 라우팅 추가

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,9 +1,17 @@
-import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { HomePage } from "@pages";
 
+/** DEV환경에서 추가되는 router */
+let DevRouter: RouteObject[];
+if (import.meta.env.DEV) {
+    const { DevRouter: DRouter } = await import("./DevRouter");
+    DevRouter = DRouter;
+}
+
 export function AppRouter() {
     const router = createBrowserRouter([
+        ...(import.meta.env.DEV ? DevRouter : []),
         {
             path: "/",
             element: <HomePage />,
---
+++ src/pages/Example/ui/ExampleSuccess.tsx
@@ -0,0 +1,7 @@
+export const ExampleSuccess = () => {
+    return (
+        <div>
+            <div>테스트 페이지</div>
+        </div>
+    );
+};
