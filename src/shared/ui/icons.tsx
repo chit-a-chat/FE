5e1e724c5c9922170a5e2ae793d19889114e6518
hani2057 <hanikim2057@gmail.com>
2024-08-12 00:55:25 +0900

    [refactor] icons 객체 function call 외부로 위치 변경

--- src/shared/ui/icons.tsx
+++ src/shared/ui/icons.tsx
@@ -17,37 +17,40 @@ import {
     IconX,
 } from "@tabler/icons-react";
 
-interface IconsProps {
+const SIZE_TO_PIXEL = {
+    xl: 30,
+    l: 24,
+    m: 20,
+    s: 18,
+    xs: 14,
+};
+
+type TIconSize = keyof typeof SIZE_TO_PIXEL;
+
+interface CustomIconsProps {
     type: TIcon;
-    size?: "XL" | "L" | "M" | "S" | "XS";
+    size?: TIconSize;
 }
 
+const icons = {
+    "arrow-left": IconArrowLeft,
+    "arrow-right": IconArrowRight,
+    bell: IconBell,
+    check: IconCheck,
+    "chevron-down": IconChevronDown,
+    "chevron-down-fill": IconTriangleInvertedFilled,
+    "chevron-up": IconChevronUp,
+    heart: IconHeartHandshake,
+    "heart-smily": IconBrandBadoo,
+    location: IconMapPin,
+    search: IconSearch,
+    setting: IconSettings,
+    user: IconUser,
+    x: IconX,
+};
+
 // TODO: color prop 받아서 적용, size prop 반복 제거. 2024.08.03. 김하늬
-export function Icons({ type, size = "M" }: IconsProps) {
-    const SIZE_TO_PIXEL = {
-        XL: 30,
-        L: 24,
-        M: 20,
-        S: 18,
-        XS: 14,
-    };
-
-    const icons = {
-        "arrow-left": <IconArrowLeft size={SIZE_TO_PIXEL[size]} />,
-        "arrow-right": <IconArrowRight size={SIZE_TO_PIXEL[size]} />,
-        bell: <IconBell size={SIZE_TO_PIXEL[size]} />,
-        check: <IconCheck size={SIZE_TO_PIXEL[size]} />,
-        "chevron-down": <IconChevronDown size={SIZE_TO_PIXEL[size]} />,
-        "chevron-down-fill": <IconTriangleInvertedFilled size={SIZE_TO_PIXEL[size]} />,
-        "chevron-up": <IconChevronUp size={SIZE_TO_PIXEL[size]} />,
-        heart: <IconHeartHandshake size={SIZE_TO_PIXEL[size]} />,
-        "heart-smily": <IconBrandBadoo size={SIZE_TO_PIXEL[size]} />,
-        location: <IconMapPin size={SIZE_TO_PIXEL[size]} />,
-        search: <IconSearch size={SIZE_TO_PIXEL[size]} />,
-        setting: <IconSettings size={SIZE_TO_PIXEL[size]} />,
-        user: <IconUser size={SIZE_TO_PIXEL[size]} />,
-        x: <IconX size={SIZE_TO_PIXEL[size]} />,
-    };
-
-    return icons[type];
+export function Icons({ type, size = "m" }: CustomIconsProps) {
+    const NewIcon = icons[type];
+    return <NewIcon size={SIZE_TO_PIXEL[size]} />;
 }
