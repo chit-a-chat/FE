f68307897e2c8c2444fdc1b2a844eeeb54da3c26
hani <hanikim2057@gmail.com>
2024-08-21 17:55:20 +0900

    [feat] theme에 palette 추가

--- src/shared/ui/theme/emotion.d.ts
+++ src/shared/ui/theme/emotion.d.ts
@@ -2,10 +2,13 @@ import { TbuttonStatus, TbuttonVariant } from "@shared/type";
 
 import "@emotion/react";
 
+import { TLightPalette } from "./palette";
 import { TButtonShape, TSpacing } from "./theme";
 
 declare module "@emotion/react" {
     export interface Theme {
+        // TODO: theme mode에 따라 palette type 추가 2024.08.21. 김하늬
+        palette: TLightPalette;
         bgColor: string;
         button: {
             shape: TButtonShape;
