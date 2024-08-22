7eb4dde96f16d1f4c2c1dfb0b3bbb7f3a9e3b0f4
hani <hanikim2057@gmail.com>
2024-08-22 20:51:59 +0900

    [feat] Typo 컴포넌트 variant prop default 값(body/regular) 추가

--- src/widgets/components/ui/typo.ts
+++ src/widgets/components/ui/typo.ts
@@ -3,8 +3,8 @@ import { TTypoVariant } from "@shared/type";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-export const Typo = styled.p<{ variant: TTypoVariant; color?: string }>`
-    ${({ variant, color, theme }) => css`
+export const Typo = styled.p<{ variant?: TTypoVariant; color?: string }>`
+    ${({ variant = "body/regular", color, theme }) => css`
         font-size: ${theme.typo[variant].fontSize};
         line-height: ${theme.typo[variant].lineHeight};
         letter-spacing: ${theme.typo[variant].letterSpacing};
