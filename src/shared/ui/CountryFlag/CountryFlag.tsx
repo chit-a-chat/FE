ce255bf68bc271a032caa4817709c2e82d756bec
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-08-25 20:43:48 +0900

    [feat] CountryFlag - CountryFlag

---
+++ src/shared/ui/CountryFlag/CountryFlag.tsx
@@ -0,0 +1,42 @@
+/** 국기 CSS */
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+/**
+ * 국가의 영어 이름과 대응하는 ISO 3166-1-alpha-2 code 맵핑한 자료.
+ * ex. Korean -> ko, English -> en
+ * {@link https://www.iso.org/obp/ui/#search/code/}
+ */
+
+const CountryToAlpha2Map = {
+    American: "us",
+    English: "gb",
+    Japanese: "jp",
+    Korean: "kr",
+} as const;
+
+export type TCountryName = keyof typeof CountryToAlpha2Map;
+
+type TFlagProps = {
+    country: TCountryName;
+    width?: string;
+    height?: string;
+    isRound?: boolean;
+    isSquare?: boolean;
+};
+
+/**
+ * @param isRound - 원형
+ * @param isSquare - 국기 정사각형 여부
+ * @param country - 국가 이름
+ * @example
+ * <span class="fi fi-gr"></span> // 일반 국기
+ * <span class="fi fi-gr fis"></span> // 정사각형 국기
+ */
+export const CountryFlag = ({ isRound, country, width, height, isSquare = true }: TFlagProps) => {
+    return (
+        <div
+            className={`fi fi-${CountryToAlpha2Map[country]} ${isSquare ? "fis" : ""}`}
+            style={{ width, height, borderRadius: isRound ? "100%" : undefined }}
+        />
+    );
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,5 @@
 export { theme } from "./theme/theme";
 export { media } from "./theme/media";
 export { Layout } from "./Layout/Layout";
+export { CountryFlag } from "./CountryFlag/CountryFlag";
+export type { TCountryName } from "./CountryFlag/CountryFlag";
