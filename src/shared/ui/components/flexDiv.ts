e5afa0fd7df5d11441669432f850cf316092aa5c
hani2057 <hanikim2057@gmail.com>
2024-08-25 20:52:31 +0900

    [refactor] CSS prop type CSSProperties 속성으로 변경

--- src/shared/ui/components/flexDiv.ts
+++ src/shared/ui/components/flexDiv.ts
@@ -1,26 +1,12 @@
+import { CSSProperties } from "react";
+
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-type TFlexDirection = "row" | "row-reverse" | "column" | "column-reverse";
-type TJustifyContent =
-    | "inherit"
-    | "center"
-    | "start"
-    | "end"
-    | "space-between"
-    | "space-around"
-    | "space-evenly"
-    | "stretch";
-type TAlignItems =
-    | "inherit"
-    | "center"
-    | "start"
-    | "end"
-    | "space-between"
-    | "space-around"
-    | "space-evenly"
-    | "stretch";
-type TFlexWrap = "inherit" | "nowrap" | "wrap" | "wrap-reverse";
+type TFlexDirection = CSSProperties["flexDirection"];
+type TJustifyContent = CSSProperties["justifyContent"];
+type TAlignItems = CSSProperties["alignItems"];
+type TFlexWrap = CSSProperties["flexWrap"];
 
 // TODO: padding, margin 등 편의용 CSS prop 추가, 지정하지 않은 css props 받아서 적용. 2024.08.22. 김하늬
 export const FlexDiv = styled.div<{
