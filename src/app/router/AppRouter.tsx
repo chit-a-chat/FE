116ff9b10f88db900cc10e01f6bfd8d1faeb8101
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-09-29 16:55:34 +0900

    [chore] 임포트 정리 - 임포트 정리

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,15 +1,10 @@
-import { Navigate, RouteObject, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { HomePage } from "@pages/home";
 
 import { RouterErrorBoundary } from "@app/provider";
 
-/** DEV환경에서 추가되는 router */
-let DevRouter: RouteObject[];
-if (import.meta.env.DEV) {
-    const { DevRouter: DRouter } = await import("./DevRouter");
-    DevRouter = DRouter;
-}
+import { DevRouter } from "./DevRouter";
 
 export function AppRouter() {
     const router = createBrowserRouter([
