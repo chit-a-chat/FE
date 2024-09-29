d702f357135ce8234c9475eb1d150a6bc938c6b9
DongGeunYoon <donggeun_yoon@tamx.co.kr>
2024-09-29 16:57:26 +0900

    [feat] 버튼 컴포넌트 사이즈 추가 - 버튼 컴포넌트 사이즈 추가

--- src/widgets/components/ui/button.tsx
+++ src/widgets/components/ui/button.tsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
-
 import { TButtonVariant, TIcon } from "@shared/type";
 
-import { Theme, css } from "@emotion/react";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 import { Icons } from "./icons";
 
@@ -11,10 +10,47 @@ interface ButtonProps {
     label: string;
     iconLeft?: TIcon;
     iconRight?: TIcon;
-    to?: string;
     // TODO: 버튼 사이즈 'large', 'small'인 경우 추가
-    size?: "medium";
+    size?: "sm" | "md" | "lg";
 }
+const StyledButton = styled.button<{
+    size: NonNullable<ButtonProps["size"]>;
+    variant: NonNullable<ButtonProps["variant"]>;
+}>`
+    ${({ theme, size, variant }) => css`
+        padding: ${theme.button.shape.padding[size]};
+        border-radius: ${theme.button.shape.radius[size]};
+        gap: ${theme.button.shape.gap[size]};
+        cursor: ${variant === "disable" ? "auto" : "pointer"};
+        background-color: ${theme.button.color.background.default?.[variant]};
+        // border를 안쪽으로 주기 위해 boxShadow 사용함
+        box-shadow: ${theme.button.color.border.default?.[variant] ?? "none"};
+        color: ${theme.button.color.text.default?.[variant]};
+        position: relative;
+        display: flex;
+        align-items: center;
+
+        &:hover {
+            background-color: ${theme.button.color.background.hover?.[variant]};
+        }
+        &:focus:not(:active)::after {
+            content: "";
+            border: ${theme.button.color.border.focus?.[variant]};
+            position: absolute;
+            left: ${variant === "primary" || variant === "error" ? "-2px" : "0px"};
+            top: ${variant === "primary" || variant === "error" ? "-3px" : "0px"};
+            width: ${`calc(100% + ${variant === "primary" || variant === "error" ? 4 : 0}px)`};
+            height: ${`calc(100% + ${variant === "primary" || variant === "error" ? 6 : 0}px)`};
+            border-radius: ${theme.button.shape.radius[size]};
+        }
+
+        &:active {
+            background-color: ${theme.button.color.background.default?.[variant]};
+            box-shadow: ${theme.button.color.border.default?.[variant] ?? "none"};
+            color: ${theme.button.color.text.default?.[variant]};
+        }
+    `}
+`;
 
 // TODO: typography theme 적용 2024.08.03. 김하늬
 export function Button({
@@ -22,53 +58,13 @@ export function Button({
     label,
     iconLeft,
     iconRight,
-    to,
-    size = "medium",
+    size = "md",
 }: ButtonProps) {
-    const btnStyle = (theme: Theme) =>
-        css({
-            padding: theme.button.shape.padding[size],
-            borderRadius: theme.button.shape.radius[size],
-            gap: theme.button.shape.gap[size],
-            cursor: variant === "disable" ? "auto" : "pointer",
-            backgroundColor: theme.button.color.background.default?.[variant],
-            // border를 안쪽으로 주기 위해 boxShadow 사용함
-            boxShadow: theme.button.color.border.default?.[variant] ?? "none",
-            color: theme.button.color.text.default?.[variant],
-            position: "relative",
-            display: "flex",
-            alignItems: "center",
-
-            "&:hover": {
-                backgroundColor: theme.button.color.background.hover?.[variant],
-            },
-
-            "&:focus:not(:active)::after": {
-                content: "''",
-                border: theme.button.color.border.focus?.[variant],
-                position: "absolute",
-                left: variant === "primary" || variant === "error" ? "-2px" : "0px",
-                top: variant === "primary" || variant === "error" ? "-3px" : "0px",
-                width: `calc(100% + ${variant === "primary" || variant === "error" ? 4 : 0}px)`,
-                height: `calc(100% + ${variant === "primary" || variant === "error" ? 6 : 0}px)`,
-                borderRadius: theme.button.shape.radius[size],
-            },
-
-            "&:active": {
-                backgroundColor: theme.button.color.background.default?.[variant],
-                boxShadow: theme.button.color.border.default?.[variant] ?? "none",
-                color: theme.button.color.text.default?.[variant],
-            },
-        });
-
     return (
-        <>
-            {to && <Link to={to} css={btnStyle} />}
-            <button css={btnStyle}>
-                {iconLeft && <Icons type={iconLeft} size={size === "medium" ? "l" : "m"} />}
-                {label}
-                {iconRight && <Icons type={iconRight} size={size === "medium" ? "l" : "m"} />}
-            </button>
-        </>
+        <StyledButton variant={variant} size={size}>
+            {iconLeft && <Icons type={iconLeft} size={size === "md" ? "l" : "m"} />}
+            {label}
+            {iconRight && <Icons type={iconRight} size={size === "md" ? "l" : "m"} />}
+        </StyledButton>
     );
 }
