14c1f9faf1a87adcce46cfba2b8c56765d5820f2
DongGeunYoon <ehdrmsdl9999@naver.com>
2024-10-06 17:08:44 +0900

    [feat] 홈화면 추가 - 홈화면 추가

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,9 +1,55 @@
+import { useTranslation } from "react-i18next";
+
 import { HomePictureList } from "@widgets/HomePictureList";
+import { HomeStatistics } from "@widgets/HomeStatistics";
+import { Button } from "@widgets/components";
+
+import { FlexDiv, Text } from "@shared/ui";
+
+import { useTheme } from "@emotion/react";
+
+import { HomeTitleLogo } from "./component/HomeTitleLogo";
 
 export function Home() {
+    const { t } = useTranslation("home");
+    const theme = useTheme();
     return (
-        <div>
-            <HomePictureList />
-        </div>
+        <section
+            css={{
+                display: "flex",
+                paddingTop: "140px",
+                paddingBottom: "auto",
+                background: `linear-gradient(180deg, ${theme.palette.primary[1]}66 0%, ${theme.palette.blue[1]}66 83%)`,
+                flexDirection: "column",
+                flex: 1,
+            }}
+        >
+            <FlexDiv direction="column" gap={48}>
+                <FlexDiv direction="column" gap={24}>
+                    <FlexDiv direction="column" gap={24}>
+                        <FlexDiv gap={5} direction="column">
+                            <Text typoVariant="display/large" color={theme.palette.primary[7]}>
+                                {t("MainTitle")}
+                            </Text>
+                            <HomeTitleLogo />
+                        </FlexDiv>
+                        <FlexDiv>
+                            <Text
+                                typoVariant="h4/regular"
+                                color={theme.palette.grey[6]}
+                                css={{ textAlign: "center" }}
+                            >
+                                {t("SubTitle")}
+                            </Text>
+                        </FlexDiv>
+                    </FlexDiv>
+                    <Button label={t("MainButton")} iconRight="search" size="lg" />
+                </FlexDiv>
+                <FlexDiv direction="column" gap={24}>
+                    <HomeStatistics />
+                    <HomePictureList />
+                </FlexDiv>
+            </FlexDiv>
+        </section>
     );
 }
