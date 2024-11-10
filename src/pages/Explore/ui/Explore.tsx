3f052f8bd3ca5f31eeab01378a644a9a0ffcbf11
DongGeunYoon <ehdrmsdl9999@naver.com>
2024-11-10 11:46:19 +0900

    [feat] Explore 페이지 - Explore 페이지

---
+++ src/pages/Explore/ui/Explore.tsx
@@ -0,0 +1,56 @@
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+import { ExploreDetails } from "@widgets/ExploreDetails";
+import { Button } from "@widgets/components";
+
+import { Icon } from "@shared/Icon";
+import { FlexDiv, Text } from "@shared/ui";
+
+import { useTheme } from "@emotion/react";
+
+export const Explore = () => {
+    const theme = useTheme();
+    const navigate = useNavigate();
+    const { t } = useTranslation("explore");
+    return (
+        <article
+            css={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "30px 60px",
+                gap: "10px",
+                backgroundColor: theme.palette.primary[0],
+                flex: 1,
+                minWidth: 0,
+            }}
+        >
+            <FlexDiv
+                direction="row"
+                gap={5}
+                alignItems="center"
+                css={{ cursor: "pointer", width: "fit-content" }}
+                onClick={() => {
+                    navigate("/");
+                }}
+            >
+                <Icon type="arrow-left" color={theme.palette.primary[5]} />
+                <Text typoVariant="label/regular" color={theme.palette.primary[5]}>
+                    {t("BackHomeButton")}
+                </Text>
+            </FlexDiv>
+            <FlexDiv direction="row" justifyContent="space-between" alignItems="center">
+                <Text typoVariant="h1/bold" color={theme.palette.common.black}>
+                    {t("Title")}
+                </Text>
+                <Button
+                    iconLeft={{ icon: "adjustments-horizontal", color: theme.palette.primary[6] }}
+                    variant="secondary"
+                    label={t("PreferenceButton")}
+                    size="md"
+                />
+            </FlexDiv>
+            <ExploreDetails />
+        </article>
+    );
+};
