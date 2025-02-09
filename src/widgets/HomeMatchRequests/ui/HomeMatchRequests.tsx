59e589b40ab89a3dc30d9ce79eaabbe8fb389f9b
DongGeunYoon <ehdrmsdl9999@naver.com>
2025-02-09 20:25:04 +0900

    [feat] 매칭요청/좋아요 화면 추가 - 매칭요청/좋아요 화면 추가

--- src/widgets/HomeMatchRequests/ui/HomeMatchRequests.tsx
+++ src/widgets/HomeMatchRequests/ui/HomeMatchRequests.tsx
@@ -1,74 +1,109 @@
 import { useTranslation } from "react-i18next";
 
-import { MatchRequestList, useMatchRequests } from "@entities/match";
+import { MatchLikeList, MatchRequestList, useMatchRequests } from "@entities/match";
 
-import { Badge, FlexDiv, Text } from "@shared/ui";
+import { Badge, FlexDiv, Tabs, Text } from "@shared/ui";
 
 import { useTheme } from "@emotion/react";
 import { useQuery } from "@tanstack/react-query";
 
 export const HomeMatchRequests = () => {
     const theme = useTheme();
-    const { getMatchRequests } = useMatchRequests();
+    const { getMatchRequests, getLikeList } = useMatchRequests();
     const { data: matchRequests } = useQuery({
         queryKey: ["test23"],
         queryFn: getMatchRequests,
         initialData: { matchRequests: [] },
         initialDataUpdatedAt: 0,
     });
+    const { data: likeList } = useQuery({
+        queryKey: ["test56"],
+        queryFn: getLikeList,
+        initialData: { likeList: [] },
+        initialDataUpdatedAt: 0,
+    });
     const { t } = useTranslation("home");
     return (
-        <FlexDiv
-            direction="column"
-            gap={10}
-            css={{
-                flex: 1,
-                paddingTop: "10px",
-                backgroundColor: theme.palette.common.white,
-                boxShadow: theme.shadow.cardShadow,
-                borderRadius: "10px",
-                position: "relative",
-            }}
-        >
-            <FlexDiv alignItems="center" gap={10} css={{ paddingLeft: "20px" }}>
-                <Text typoVariant="h3/medium" color={theme.palette.common.black}>
-                    {t("UpdateTitle")}
-                </Text>
+        <Tabs value={"requests"}>
+            <FlexDiv
+                direction="column"
+                gap={10}
+                css={{
+                    flex: 1,
+                    paddingTop: "10px",
+                    backgroundColor: theme.palette.common.white,
+                    boxShadow: theme.shadow.cardShadow,
+                    borderRadius: "10px",
+                    position: "relative",
+                }}
+            >
+                <FlexDiv alignItems="center" gap={10} css={{ paddingLeft: "20px" }}>
+                    <Text typoVariant="h3/medium" color={theme.palette.common.black}>
+                        {t("UpdateTitle")}
+                    </Text>
 
-                <Text
-                    css={{
-                        padding: "5px 12px",
-                        gap: "10px",
-                        backgroundColor: theme.palette.primary[0],
-                        borderRadius: "30px",
-                    }}
-                    typoVariant="tag/regular"
-                    color={theme.palette.common.black}
-                >
-                    {matchRequests.matchRequests.length}
-                </Text>
+                    <Text
+                        css={{
+                            padding: "5px 12px",
+                            gap: "10px",
+                            backgroundColor: theme.palette.primary[0],
+                            borderRadius: "30px",
+                        }}
+                        typoVariant="tag/regular"
+                        color={theme.palette.common.black}
+                    >
+                        {matchRequests.matchRequests.length + likeList.likeList.length}
+                    </Text>
+                </FlexDiv>
+                <FlexDiv direction="row" gap={10} css={{ paddingLeft: "20px" }}>
+                    <Tabs.Tab
+                        value={"requests"}
+                        css={{
+                            "& > div": {
+                                boxShadow: theme.shadow.tabCommonShadow,
+                            },
+                            "&[data-selected=true] > div": {
+                                boxShadow: theme.shadow.tabPurpleShadow,
+                            },
+                        }}
+                    >
+                        <Badge
+                            size="lg"
+                            radius="full"
+                            fontVariant="tag/regular"
+                            color={theme.palette.common.black}
+                        >
+                            {t("MatchRequestBadge")}
+                        </Badge>
+                    </Tabs.Tab>
+                    <Tabs.Tab
+                        value={"likes"}
+                        css={{
+                            "& > div": {
+                                boxShadow: theme.shadow.tabCommonShadow,
+                            },
+                            "&[data-selected=true] > div": {
+                                boxShadow: theme.shadow.tabPurpleShadow,
+                            },
+                        }}
+                    >
+                        <Badge
+                            size="lg"
+                            radius="full"
+                            fontVariant="tag/regular"
+                            color={theme.palette.common.black}
+                        >
+                            {t("ReceivedLikeBadge")}
+                        </Badge>
+                    </Tabs.Tab>
+                </FlexDiv>
+                <Tabs.Content value={"requests"}>
+                    <MatchRequestList />
+                </Tabs.Content>
+                <Tabs.Content value={"likes"}>
+                    <MatchLikeList />
+                </Tabs.Content>
             </FlexDiv>
-            <FlexDiv direction="row" gap={10} css={{ paddingLeft: "20px" }}>
-                <Badge
-                    size="lg"
-                    radius="full"
-                    fontVariant="tag/regular"
-                    color={theme.palette.common.black}
-                    isShadow
-                >
-                    {t("MatchRequestBadge")}
-                </Badge>
-                <Badge
-                    size="lg"
-                    radius="full"
-                    fontVariant="tag/regular"
-                    color={theme.palette.common.black}
-                    isShadow
-                >
-                    {t("ReceivedLikeBadge")}
-                </Badge>
-            </FlexDiv>
-            <MatchRequestList />
-        </FlexDiv>
+        </Tabs>
     );
 };
